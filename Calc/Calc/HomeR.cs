95fb0bb31c24055d740a89aac0b32bf042c1f8e2
kolva <kol.vasilencko2017@yandex.ru>
2020-11-21 13:18:40 +0300

    Create Calculator

---
+++ Calc/Calc/HomeR.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            Int16  a, b, c;
+
+            str = Console.ReadLine();
+            a = Convert.ToChar(str);
+
+            str = Console.ReadLine();
+            b = Convert.ToChar(str);
+
+            str = Console.ReadLine();
+            c = Convert.ToChar(str);
+
+            Char resultOne = a + b + c;
+            Char resultTwo = a * b * c;
+
+            Console.WriteLine("Сумма чисел =", resultOne);
+            Console.WriteLine("Произведение  чисел =", resultTwo);
---
+++ Calc/Calc/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc
+{
+    class Calculator
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Если хотите выйте из калькулятора нажмите Escape");
+            while (Console.ReadKey(true).Key != ConsoleKey.Escape)
+            { 
+                Console.Clear();
+                double NumberOne, NumberTwo;
+                string Operation;
+
+                try
+                {
+                    Console.WriteLine("Введите 1 число");
+                    NumberOne = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Введите 2 число");
+                    NumberTwo = double.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Введено отнюдь не число");
+                    Console.ReadLine();
+                    continue;   
+                }
+
+                
+                Console.WriteLine("Какую операцию хотите совершить над этими числами");
+                Operation = Console.ReadLine();
+                switch (Operation)
+                {
+                    case "+":
+                        Console.WriteLine(NumberOne + NumberTwo);
+                        break;
+                    case "-":
+                        Console.WriteLine(NumberOne - NumberTwo);
+                        break;
+                    case "/":
+                        Console.WriteLine(NumberOne / NumberTwo);
+                        break;
+                    case "*":
+                        Console.WriteLine(NumberOne * NumberTwo);
+                        break;
+                    case "%":
+                        Console.WriteLine(NumberOne % NumberTwo);
+                        break;
+                    default:
+                        Console.WriteLine("Такой операции не существует в калькуляторе");
+                        break;
+                }
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
